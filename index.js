38646dff8b9c322ce1a7c9b5f83cbdecf8afcf46
Olha Melska <olya.melska@gmail.com>
2018-08-08 16:19:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ main.remove()
 const newHeader = document.createElement('h1')
 document.body.appendChild(newHeader)
 newHeader.setAttribute("id", "victory")
-newHeader.innerText = 'Olya is a champion!'
+newHeader.innerText = 'Olya is the champion!'
